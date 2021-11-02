0451b4d150fa4597e317315efb9e3ca94baa6cf0
arulabdulaziz <rulz.matrixs@gmail.com>
2021-11-03 06:48:59 +0700

    display list liga and init add liga

--- src/views/ListLiga/LigaAdd.js
+++ src/views/ListLiga/LigaAdd.js
@@ -7,15 +7,23 @@ import {
   Row,
 } from "reactstrap";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 const LigaAdd = (props) => {
   return (
     <div className="content">
       <Row>
+        <Col>
+          <Link to="/admin/liga" className="btn btn-link p-2">
+            <i className="nc-icon nc-minimal-left mr-1"></i>
+            Kembali
+          </Link>
+        </Col>
+      </Row>
+      <Row>
         <Col md="12">
           <Card>
             <CardHeader tag="h4">Tambah Liga</CardHeader>
-            <CardBody>
-            </CardBody>
+            <CardBody></CardBody>
           </Card>
         </Col>
       </Row>
