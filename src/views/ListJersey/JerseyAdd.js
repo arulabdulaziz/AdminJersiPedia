050f94a0103694854b0d42a5f64d583cce340e7b
arulabdulaziz <rulz.matrixs@gmail.com>
2021-11-09 06:35:20 +0700

    init add liga

--- src/views/ListJersey/JerseyAdd.js
+++ src/views/ListJersey/JerseyAdd.js
@@ -10,6 +10,7 @@ import {
   Button,
   Spinner,
   Progress,
+  Label,
 } from "reactstrap";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
@@ -18,10 +19,51 @@ import swal from "sweetalert";
 import FIREBASE from "../../config/FIREBASE";
 const JerseyAdd = (props) => {
   const [image, setImage] = useState(DefaultImage);
+  const [image2, setImage2] = useState(DefaultImage);
   const [name, setName] = useState("");
   const [imageToDb, setImageToDb] = useState(null);
+  const [imageToDb2, setImageToDb2] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [loadingRender, setLoadingRender] = useState(false);
   const [progress, setProgress] = useState(0);
+  const [sizeSelected, setSizeSelected] = useState([]);
+  const [ligas, setLigas] = useState([]);
+  const [liga, setLiga] = useState("");
+  const [price, setPrice] = useState(0);
+  const [isReady, setIsReady] = useState(true);
+  const [weight, setWeight] = useState(0);
+  const [type, setType] = useState("");
+  const sizes = ["S", "M", "L", "XL", "XXL"];
+  useEffect(() => {
+    getAllLiga();
+  }, []);
+  const getAllLiga = () => {
+    setLoadingRender(true);
+    FIREBASE.database()
+      .ref("ligas")
+      .once("value", (querySnapShot) => {
+        let value = querySnapShot.val() ? querySnapShot.val() : null;
+        if (!value) value = [];
+        else {
+          value = Object.keys(value).map((e) => ({
+            ...value[e],
+            uid: e,
+            loading: false,
+          }));
+        }
+        // console.log(value, "<<< value ligas");
+        // dispatchSuccess(dispatch, GET_LIST_LIGA, value);
+        setLigas(value);
+      })
+      .catch((err) => {
+        console.log("Error: ", JSON.stringify(err));
+        // // dispatchError(dispatch, GET_LIST_LIGA, JSON.stringify(err), []);
+        // setErrorLigas(JSON.stringify(err));
+      })
+      .finally((_) => {
+        setLoadingRender(false);
+      });
+  };
   const submit = () => {
     if (name && imageToDb) {
       //upload ke storage firebase
@@ -108,85 +150,253 @@ const JerseyAdd = (props) => {
           </Link>
         </Col>
       </Row>
-      <Row>
-        <Col md="12">
-          <Card>
-            <CardHeader tag="h4">Tambah Liga</CardHeader>
-            <CardBody>
-              <img src={image} width="200" />
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  submit();
-                }}
-              >
-                <Row className="align-items-center">
-                  <Col md={6}>
-                    <FormGroup>
-                      <label>Logo Liga</label>
-                      <Input
-                        type="file"
-                        accept=".jpg, .png, .jpeg"
-                        id="img"
-                        onChange={(event) => {
-                          var file = event.target.files[0];
-                          var t = file.type.split("/").pop().toLowerCase();
-                          if (
-                            t != "jpeg" &&
-                            t != "jpg" &&
-                            t != "png" &&
-                            t != "bmp" &&
-                            t != "gif"
-                          ) {
-                            event.target.value = "";
-                            setImage(DefaultImage);
-                            setImageToDb(null);
-                          } else {
-                            setImage(URL.createObjectURL(file));
-                            setImageToDb(file);
-                          }
-                        }}
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col md={6}>
-                    <FormGroup>
-                      <label>Nama Liga</label>
-                      <Input
-                        type="text"
-                        value={name}
-                        onChange={(event) => {
-                          setName(event.target.value);
-                        }}
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <Button type="submit" color="primary" disabled={loading}>
-                      {loading && (
-                        <Spinner
-                          color="white"
-                          size="sm"
-                          className="mr-2"
-                        ></Spinner>
-                      )}
-                      <span>Submit</span>
-                    </Button>
-                  </Col>
-                </Row>
-              </form>
-              {loading && (
-                <>
-                  <Progress value={progress} />{" "}
-                  <span>{Math.floor(+progress)}%</span>
-                </>
-              )}
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+      {loadingRender ? (
+        <Card>
+          <CardBody className="text-center">
+            <Spinner color="primary" className=""></Spinner>
+          </CardBody>
+        </Card>
+      ) : (
+        <Row>
+          <Col md="12">
+            <Card>
+              <CardHeader tag="h4">Tambah Jersey</CardHeader>
+              <CardBody>
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    submit();
+                  }}
+                >
+                  <Row className="">
+                    <Col md={6}>
+                      <Row>
+                        <Col>
+                          <img src={image} width="200" />
+                          <FormGroup>
+                            <label>Foto Jersey (Depan)</label>
+                            <Input
+                              type="file"
+                              accept=".jpg, .png, .jpeg"
+                              id="img"
+                              onChange={(event) => {
+                                var file = event.target.files[0];
+                                var t = file.type
+                                  .split("/")
+                                  .pop()
+                                  .toLowerCase();
+                                if (
+                                  t != "jpeg" &&
+                                  t != "jpg" &&
+                                  t != "png" &&
+                                  t != "bmp" &&
+                                  t != "gif"
+                                ) {
+                                  event.target.value = "";
+                                  setImage(DefaultImage);
+                                  setImageToDb(null);
+                                } else {
+                                  setImage(URL.createObjectURL(file));
+                                  setImageToDb(file);
+                                }
+                              }}
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col>
+                          <img src={image2} width="200" />
+                          <FormGroup>
+                            <label>Foto Jersey (Belakang)</label>
+                            <Input
+                              type="file"
+                              accept=".jpg, .png, .jpeg"
+                              id="img"
+                              onChange={(event) => {
+                                var file = event.target.files[0];
+                                var t = file.type
+                                  .split("/")
+                                  .pop()
+                                  .toLowerCase();
+                                if (
+                                  t != "jpeg" &&
+                                  t != "jpg" &&
+                                  t != "png" &&
+                                  t != "bmp" &&
+                                  t != "gif"
+                                ) {
+                                  event.target.value = "";
+                                  setImage2(DefaultImage);
+                                  setImageToDb2(null);
+                                } else {
+                                  setImage2(URL.createObjectURL(file));
+                                  setImageToDb2(file);
+                                }
+                              }}
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col md={6}>
+                      <FormGroup>
+                        <label>Nama Jersey</label>
+                        <Input
+                          // placeholder="Nama Jersey"
+                          type="text"
+                          value={name}
+                          onChange={(event) => {
+                            setName(event.target.value);
+                          }}
+                        />
+                      </FormGroup>
+                      <Row>
+                        <Col md="6">
+                          <FormGroup>
+                            <label>Liga</label>
+                            <Input
+                              type="select"
+                              name="liga"
+                              value={liga}
+                              onChange={(event) => setLiga(event.target.value)}
+                            >
+                              <option value="">--Pilih--</option>
+                              {ligas.map((e) => (
+                                <option value={e.uid} key={e.uid}>
+                                  {e.liga_name}
+                                </option>
+                              ))}
+                            </Input>
+                          </FormGroup>
+                        </Col>
+                        <Col md="6">
+                          <FormGroup>
+                            <label>Harga</label>
+                            <Input
+                              type="number"
+                              // placeholder="Harga"
+                              value={price}
+                              onChange={(event) => {
+                                if (+event.target.value < 0) setPrice(0);
+                                setPrice(+event.target.value);
+                              }}
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+
+                      <Row>
+                        <Col md={6}>
+                          <FormGroup>
+                            <label>Berat (kg)</label>
+                            <Input
+                              type="number"
+                              value={weight}
+                              name="weight"
+                              // placeholder="Berat"
+                              onChange={(event) => {
+                                if (+event.target.value < 0) setWeight(0);
+                                setWeight(+event.target.value);
+                              }}
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                          <FormGroup>
+                            <label>Jenis</label>
+                            <Input
+                              type="text"
+                              value={type}
+                              name="type"
+                              // placeholder="Jenis"
+                              onChange={(event) => setType(event.target.value)}
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+
+                      <Row>
+                        <Col md={6}>
+                          <label>Ukuran</label>
+                          <FormGroup check>
+                            {sizes.map((e, index) => (
+                              <Label key={index} check className="mr-2 mb-1">
+                                <Input
+                                  type="checkbox"
+                                  value={e}
+                                  onChange={(event) => {
+                                    const checked = event.target.checked;
+                                    const value = event.target.value;
+                                    if (checked) {
+                                      //jika user ceklis ukuran
+                                      //isi state array ukuran selected
+                                      setSizeSelected([...sizeSelected, value]);
+                                    } else {
+                                      //jika user menghapus ceklis ukuran
+                                      const newSize = sizeSelected
+                                        .filter((size) => size !== value)
+                                        .map((e) => e);
+                                      setSizeSelected([newSize]);
+                                    }
+                                  }}
+                                />
+                                {e}
+                                <span className="form-check-sign">
+                                  <span className="check"></span>
+                                </span>
+                              </Label>
+                            ))}
+                          </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                          <FormGroup>
+                            <label>Ready</label>
+                            <Input
+                              type="select"
+                              name="ready"
+                              value={isReady}
+                              onChange={(event) =>
+                                setIsReady(event.target.value)
+                              }
+                            >
+                              <option value={true}>Ada</option>
+                              <option value={false}>Kosong</option>
+                            </Input>
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Button
+                        type="submit"
+                        color="primary"
+                        disabled={loading}
+                        className="float-right"
+                      >
+                        {loading && (
+                          <Spinner
+                            color="white"
+                            size="sm"
+                            className="mr-2"
+                          ></Spinner>
+                        )}
+                        <span>Submit</span>
+                      </Button>
+                    </Col>
+                  </Row>
+                </form>
+                {loading && (
+                  <>
+                    <Progress value={progress} />{" "}
+                    <span>{Math.floor(+progress)}%</span>
+                  </>
+                )}
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </div>
   );
 };
